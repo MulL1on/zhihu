6bf59ecfa74f7cae7dcfb84cbc82d8c43f971284
lianwgeijian666 <liangweijian666@outlook.com>
2023-01-25 16:48:34 +0800

    fix jwt

--- app/internal/middleware/jwt.go
+++ app/internal/middleware/jwt.go
@@ -1,8 +1,8 @@
 package middleware
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9"
 	"github.com/golang-jwt/jwt/v4"
 	"go.uber.org/zap"
 	g "juejin/app/global"
@@ -28,6 +28,21 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+
+		//check black list
+		if err := g.Rdb.Get(c, token).Err(); err != nil {
+			if err != redis.Nil {
+				resp.ResponseFail(c, http.StatusInternalServerError, "get token black list fail")
+				c.Abort()
+				return
+			}
+		} else {
+			resp.ResponseFail(c, http.StatusUnauthorized, "token is invalid")
+			c.Abort()
+			return
+		}
+
+		//parse token
 		jwtConfig := g.Config.Middleware.Jwt
 		j := myjwt.NewJWT(&myjwt.Config{SecretKey: jwtConfig.SecretKey})
 		mc, err := j.ParseToken(token)
@@ -39,17 +54,17 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 
 		//refresh token
 		if mc.ExpiresAt.Unix()-time.Now().Unix() < mc.BufferTime && mc.ExpiresAt.Unix()-time.Now().Unix() > 0 {
-			mc.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Duration(g.Config.Middleware.Jwt.ExpiresTime) * time.Second))
-			newToken, _ := j.GenerateToken(mc)
-			newClaims, _ := j.ParseToken(newToken)
-			cookieWriter.Set("x-token", newToken)
-			err = g.Rdb.Set(c, fmt.Sprintf("jwt:%d", newClaims.BaseClaims.Id), newToken, time.Duration(jwtConfig.ExpiresTime)*time.Second).Err()
+			//将旧的token放进黑名单
+			err = g.Rdb.Set(c, token, "", time.Duration(mc.ExpiresAt.Unix()-time.Now().Unix())*time.Second).Err()
 			if err != nil {
-				g.Logger.Error("set redis key failed", zap.Error(err), zap.String("key", "jwt[:id]"), zap.Int64("id", newClaims.BaseClaims.Id))
-				resp.ResponseFail(c, http.StatusInternalServerError, "set token failed")
+				g.Logger.Error("set redis key failed", zap.Error(err))
+				resp.ResponseFail(c, http.StatusInternalServerError, "set token  to blacklist failed")
 				c.Abort()
 				return
 			}
+			mc.ExpiresAt = jwt.NewNumericDate(time.Now().Add(time.Duration(g.Config.Middleware.Jwt.ExpiresTime) * time.Second))
+			newToken, _ := j.GenerateToken(mc)
+			cookieWriter.Set("x-token", newToken)
 		}
 		c.Set("id", mc.BaseClaims.Id)
 		c.Next()
